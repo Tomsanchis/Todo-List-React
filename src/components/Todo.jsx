31319e7fc67a75e1eefa1b33c7f3377d9c448bfc
Tom <tsanchis@edenschool.fr>
2022-06-18 01:02:30 +0200

    add

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const Todo = (props) => {
   return (
     <div className="card">
-      <div>
-        <p>{props.todo.content}</p>
-        <p>{props.todo.id}</p>
-      </div>
+        <div>
+          <label htmlFor="checker"></label>
+          <input type="checkbox" name="checker" id="checker"/>
+          <p>{props.todo.content}</p>
+        </div>
+        <div>  
+          <p className="date">{props.todo.id}</p>
+        </div>
     </div>
   );
 };
